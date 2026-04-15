682413016530e7ee437029da7049cc33f453c025
himanshisarode <himanshis@iisc.ac.in>
2026-04-15 16:19:04 +0530

    idk2

--- real-nvp-master/util/__init__.py
+++ real-nvp-master/util/__init__.py
@@ -1,4 +1,46 @@
-from util.array_util import squeeze_2x2, checkerboard_mask
-from util.norm_util import get_norm_layer, get_param_groups, WNConv2d
-from util.optim_util import bits_per_dim, clip_grad_norm
-from util.shell_util import AverageMeter
+# =========================
+# ARRAY UTILITIES (MASKS + SQUEEZE)
+# =========================
+
+from util.array_util import (
+    squeeze_2x2,
+
+    # Spatial masks
+    checkerboard_mask,
+    diagonal_mask,
+    vertical_mask,
+    horizontal_mask,
+    quadrant_mask,
+    border_mask,
+    alternate_border_mask
+)
+
+
+# =========================
+# NORMALIZATION UTILITIES
+# =========================
+
+from util.norm_util import (
+    get_norm_layer,
+    get_param_groups,
+    WNConv2d
+)
+
+
+# =========================
+# OPTIMIZATION UTILITIES
+# =========================
+
+from util.optim_util import (
+    bits_per_dim,
+    clip_grad_norm
+)
+
+
+# =========================
+# SHELL / LOGGING UTILITIES
+# =========================
+
+from util.shell_util import (
+    AverageMeter
+)
